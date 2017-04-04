c1efacc49033073f80950682c1061c931fe8d819
lRedOx <descourieres.kevin@gmail.com>
2017-04-04 17:06:32 +0200

    04/04/2017-17:00

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css"> </head>
-	<link rel="stylesheet" href="./css/style.css"> </head>
+    <title> ALASSO </title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="./css/style.css"> 
+	
+	<!-- Load Leaflet from CDN-->
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
-	<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+	<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet-src.js"></script>
+	
+	<!-- Load Esri Leaflet from CDN -->
+	<script src="https://unpkg.com/esri-leaflet@2.0.8"></script>
 	<script src="./js/leaflet-providers.js"></script>
+
+</head>
 
 <body>
     <div id="wrapper">
@@ -57,10 +64,117 @@
 						attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 						}).addTo(mymap);
 						
-						var marker = L.marker([43.611266, 3.87924]).addTo(mymap);
+						var greenIcon = new L.Icon({
+						  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+						  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+						  iconSize: [25, 41],
+						  iconAnchor: [12, 41],
+						  popupAnchor: [1, -34],
+						  shadowSize: [41, 41]
+						});
 				
 					</script>
 				</div>
+				
+				<div id="filtre">
+						<form method="post" action="index.php">
+							<div id="themes">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+								<input type="checkbox" name="domaine" value="Bike">
+							</div>
+							
+							<div id="query" class="leaflet-bar">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">
+								<input type="checkbox" name="quartier" value="Bike">	
+							</div>
+							
+							<div id="query" class="leaflet-bar">
+								<input type="checkbox" name="public1" value="Bike">
+								<input type="checkbox" name="public2" value="Bike">
+								<input type="checkbox" name="public3" value="Bike">	
+							</div>
+						</form>
+				</div>
+				
+<?php
+
+	function Affiche_Marker(){
+		
+		$domaine = $_POST['domaine']
+		$quartier = $_POST['quartier']
+		
+		$bdd = new PDO('mysql:host=localhost;dbname=projetrni','root',''); 
+		
+			foreach ($domaine as $filtre1) {
+				foreach ($quartier as $filtre2) {
+		
+					$sql = "SELECT *  FROM assos2017 WHERE Domaine =".$filtre1."AND Code_Quartier =".$filtre2."";
+					$req = $bdd->query($sql);
+					
+					echo "<script>";
+					
+					foreach ($req as $line) {
+						if($line['Domaine'] == "" OR $line['Domaine'] == "" OR $line['Domaine'] == ""){
+							echo "var marker = L.marker([".$line[''].", ".$line['']."],{icon: greenIcon}).addTo(mymap);";
+							echo 'marker.bindPopup("<b>'.$line[''].'</b><br>'.$line[''].'");';
+						}
+						elseif($line['Domaine'] == "" OR $line['Domaine'] == "" OR $line['Domaine'] == ""){
+							echo "var marker = L.marker([".$line[''].", ".$line[''].").addTo(mymap);";
+							echo 'marker.bindPopup("<b>'.$line[''].'</b><br>'.$line[''].'");';
+						}
+						else{
+							echo "var marker = L.marker([".$line[''].", ".$line[''].").addTo(mymap);";
+							echo 'marker.bindPopup("<b>'.$line[''].'</b><br>'.$line[''].'");';
+						}
+					}
+					
+					echo "</script>";
+					
+				}
+			}
+	}
+	
+	function Affiche_Liste(){
+		
+		$domaine = $_POST['domaine']
+		$quartier = $_POST['quartier']
+		
+		$bdd = new PDO('mysql:host=localhost;dbname=projetrni','root',''); 
+		
+			foreach ($domaine as $filtre1) {
+				foreach ($quartier as $filtre2) {
+		
+					$sql = "SELECT *  FROM assos2017 WHERE Domaine =".$filtre1."AND Code_Quartier =".$filtre2."";
+					$req = $bdd->query($sql);
+					
+					foreach ($req as $line) {
+						
+						echo"<p>ID ASSO :".$line['CSASSO']."<br>";
+						echo"<b>NOM :".$line['Nom_Asso']."<br>";
+						echo"TEL :".$line['Tel_Asso']."<br>";
+						echo"MAIL :".$line['Mail_Asso']."<br>";
+						echo"SITE :".$line['Int_Asso']."<br>";
+						
+					}
+				}
+			}				
+
+?>
+				
             </div>
         </div>
         <!-- /#page-content-wrapper -->
